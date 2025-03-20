1f996e5bb0c5d7593e242d4f7f5bd2f4b1b4771a
Udeh Tochukwu Tobias <jaxj9449@gmail.com>
2025-03-20 21:58:23 +0100

    fixed the problem

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
-  return <main className="flex justify-center items-center h-screen">
+  return <main className="flex justify-center items-center h-screen bg-slate-400">
     <ConnectButton />
   </main>
 }
